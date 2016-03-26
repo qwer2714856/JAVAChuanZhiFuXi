10972da3432d1e5b68bd7385d55e9d59c92e9f8a
luchao.ding <116710782@qq.com>
2016-03-26 13:29:47 +0800

    '传智总复习.doc文档'

--- 文档总复习/src/always_revision/BaseDescription.java
+++ 文档总复习/src/always_revision/BaseDescription.java
@@ -1,5 +1,11 @@
 /**
  * 基础知识的复习 -- 来自传智播客最后所有的文档 24天的
+ * 
+ * 第一章 计算机入门
+ *     | 
+ *     |
+ *     |
+ * 
  */
 package always_revision;
 
@@ -14,7 +20,7 @@ public class BaseDescription {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
