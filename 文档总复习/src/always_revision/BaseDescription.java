47d51422f8fbf3117df22ef032507d1cfb8574fa
luchao.ding <116710782@qq.com>
2016-03-26 11:57:41 +0800

    'BaseDescription'

---
+++ 文档总复习/src/always_revision/BaseDescription.java
@@ -0,0 +1,20 @@
+/**
+ * 基础知识的复习 -- 来自传智播客最后所有的文档 24天的
+ */
+package always_revision;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class BaseDescription {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
