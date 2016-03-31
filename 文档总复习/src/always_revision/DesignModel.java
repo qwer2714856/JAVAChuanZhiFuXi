ff0ef34810a09b36823cfe1249130b2717c1425f
luchao.ding <116710782@qq.com>
2016-03-31 11:49:48 +0800

    'base'

---
+++ 文档总复习/src/always_revision/DesignModel.java
@@ -0,0 +1,25 @@
+/**
+ * 设计模式
+ * 什么是设计模式？
+ * 就是前人总结出来的为了解决一类问题的流程。
+ * 
+ * 单例设计模式
+ * 
+ */
+package always_revision;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class DesignModel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
