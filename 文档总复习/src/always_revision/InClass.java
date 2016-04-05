d2e4f613a5f59058ebc8c78779cb7b3c5db03616
luchao.ding <116710782@qq.com>
2016-04-05 09:48:38 +0800

    'inclass'

---
+++ 文档总复习/src/always_revision/InClass.java
@@ -0,0 +1,21 @@
+/**
+ * ÄÚ²¿Àà
+ * 
+ */
+package always_revision;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class InClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
