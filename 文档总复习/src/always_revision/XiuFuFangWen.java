0e8fccfd6a11b2b5846ac0f90eab1391f8e6ac96
luchao.ding <116710782@qq.com>
2016-04-06 09:23:01 +0800

    'package'

---
+++ 文档总复习/src/always_revision/XiuFuFangWen.java
@@ -0,0 +1,28 @@
+/**
+ * 类的权限修饰符的范围
+ * ------------------------------------------
+ * 				public	protected	default	private
+ * 同类			√		√			√		√
+ * 同包			√		√			√		×
+ * 子类			√		√			√		×
+ * 不同包			√		×			×		×
+ * 不同包子类		√		√			×		×
+ * ------------------------------------------
+ */
+package always_revision;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class XiuFuFangWen {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
