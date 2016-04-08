bdd0b9bb9579616ec06b780fd7eebe6ca5087c02
luchao.ding <116710782@qq.com>
2016-04-08 08:33:43 +0800

    'jihe'

---
+++ 文档总复习/src/always_revision/JieHe.java
@@ -0,0 +1,26 @@
+/**
+ * 集合
+ * 什么是集合
+ * 存储对象的容器，面向对象语言操作的都是对象，所以为了操作多个对象，存储对象出现了集合。	
+ * 集合的出现是为了存储对象，集合可以存储任意对象，长度不固定。 数组必须存储指定类型和固定长度。当存储基本数据类型用的是拆箱。
+ * 
+ */
+package always_revision;
+
+import java.util.ArrayList;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class JieHe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 
+	}
+
+}
