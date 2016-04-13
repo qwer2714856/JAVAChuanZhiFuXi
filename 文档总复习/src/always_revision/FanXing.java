efd22c2f45f5f5bc15dc4777ef60f350e495a0ea
luchao.ding <116710782@qq.com>
2016-04-13 09:02:26 +0800

    'fanxing'

---
+++ 文档总复习/src/always_revision/FanXing.java
@@ -0,0 +1,20 @@
+/**
+ * 泛型
+ */
+package always_revision;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class FanXing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
