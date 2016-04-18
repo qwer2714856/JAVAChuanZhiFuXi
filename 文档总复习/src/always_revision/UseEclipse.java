30a3f30e921ff4b4031c528d2331f539a6b437ad
luchao.ding <116710782@qq.com>
2016-04-18 10:07:43 +0800

    'eclipse_class'

--- 文档总复习/src/always_revision/UseEclipse.java
+++ 文档总复习/src/always_revision/UseEclipse.java
@@ -33,7 +33,20 @@
  * ctrl + d 								删除当前行
  * ctrl + h 								搜索全局的
  * 
+ * 自定义快捷键
+ * 设置自定义快捷键
+ * window->preferences->搜索keys->在右侧的搜索栏中输入快捷键可产看是否已经使用，或者搜一些关键词，
+ * 例如setter 这个是设置set 或 get的，设置constructor 是设置构造方法的，带fields的是带参的.剩下的那个是不带参的(SuperClass).
+ * unbind command 解除绑定
+ * binding 		     绑定的意思
  * 
+ * 类的创建
+ * 方法、构造函数，封装
+ * 1在类上{里面空白地方} 右键 source->可以创建一些东西
+ * 将代码快抽成 独立的方法。
+ * 2选中代码块，右键 -> refactor-> Extract Method->输入方法名即可
+ * 3变量重命名一个文档中变量很多如何全部重命名？
+ * 选中变量 右键 refactor->rename 就可以
  * 
  */
 package always_revision;
@@ -43,13 +56,19 @@ package always_revision;
  * 
  */
 public class UseEclipse {
-
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		haha();
+	}
 
+	public static void haha() {
+		{
+			
+		}
 	}
 
 }
