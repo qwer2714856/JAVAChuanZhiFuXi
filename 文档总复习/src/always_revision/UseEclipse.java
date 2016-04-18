720ff2566054db01dd7fc2ba3c00071c7d0ac913
luchao.ding <116710782@qq.com>
2016-04-18 08:49:49 +0800

    'use_eclipse'

---
+++ 文档总复习/src/always_revision/UseEclipse.java
@@ -0,0 +1,37 @@
+/**
+ * ecplise 的使用
+ * 工作空间(worksapce)
+ * 工程(project)
+ * 
+ * 工作空间：
+ * 建议使用年月日命名，不能有中文和空格
+ * 工程：
+ * java project 管理所有的java原文件和 class文件
+ * file->new -> java project  可以选择jdk版本，和工作空间。
+ * 
+ * 运行java文件
+ * 1.ctrl + f11
+ * 2.右键源文件 run as -> java application
+ * 3.工具栏 Run -> run as -> java application
+ * 4.在package explorer 上右键源文件 -> run as -> java application
+ * 
+ * 常用快捷键
+ * 
+ */
+package always_revision;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class UseEclipse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+	}
+
+}
