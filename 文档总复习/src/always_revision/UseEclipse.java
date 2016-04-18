a7335acb2c46d8fc43e42d4cbe5f22072abbb7f1
luchao.ding <116710782@qq.com>
2016-04-18 09:22:34 +0800

    'custom_key'

--- 文档总复习/src/always_revision/UseEclipse.java
+++ 文档总复习/src/always_revision/UseEclipse.java
@@ -16,13 +16,31 @@
  * 4.在package explorer 上右键源文件 -> run as -> java application
  * 
  * 常用快捷键
+ * alt + /									内容补全
+ * ctrl + 1									快速修复
+ * ctrl + shift + o 						快速引包，没有引用的包去掉。包的清理和加入
+ * ctrl + shift + f 						代码整理
+ * alt + ↑↓									条换代码位置
+ * ctrl + / 								添加删除单行注释,热键冲突
+ * ctrl + shift + /							添加多行注释
+ * ctrl + shift + \							删除多行注释
+ * window->Reset Perspective 				还原视图当视图被搞乱
+ * ctrl + shift + x 						转大写
+ * ctrl + shift + y 						转小写
+ * ctrl + alt + 向下键						复制 行，热键冲突
+ * ctrl + 单击方法名或类名或其它。				查看源代码，注意需要有api否则看不了，src.zip里面有
+ * ctrl + shift + t 						搜索查看方法或类的源文件。
+ * ctrl + d 								删除当前行
+ * ctrl + h 								搜索全局的
+ * 
+ * 
  * 
  */
 package always_revision;
 
 /**
  * @author www.23.com
- *
+ * 
  */
 public class UseEclipse {
 
@@ -31,7 +49,7 @@ public class UseEclipse {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 
+
 	}
 
 }
