b97bc83e8b0c3e3d2f2d71d194ba60716c7d642a
luchao.ding <116710782@qq.com>
2016-04-27 08:50:27 +0800

    '装饰着设计模式'

--- 文档总复习/new/src/always_revision/DesignModel.java
+++ 文档总复习/new/src/always_revision/DesignModel.java
@@ -15,10 +15,52 @@
  * 
  * 固定的使用final 修饰方法，不固定的抽象为抽象方法让子类去实现。
  * 
+ * 装饰着模式
+ * //繁琐的实现
+ * //需求1在输出文件行的时候打印行号。
+ * 总结：
+ * 第一步
+ * 增强子类中维护者被增强类的引用。
+ * 第二步
+ * 构造初始化
+ * 第三步
+ * 增强方法调用被维护类的增强方法，这里是多态
+ * 举例
+ * 现在有三个类A B C 功能是在BufferedReader 的readLine上多打印A B C 如果维护着被增强类的引用就可以接着调用，三个类完成7个功能。
+ * A B C  AB AC BC ABC 
+ * 第一步
+ * class ZsA extends BufferedReader{
+	private BufferedReader bufferedReader;
  * 
- * 
+ * class ZsB extends BufferedReader{
+	private BufferedReader bufferedReader;
+ *
+ * class ZsC extends BufferedReader{
+	private BufferedReader bufferedReader
+ *
+ * 第二步
+ * public ZsA(BufferedReader in) {//Reader是所有字符流的基类
+		super(in);
+		this.bufferedReader = in;
+		// TODO Auto-generated constructor stub
+	}
+ * 第三步
+ * public String readLine() throws IOException{
+		String rt = bufferedReader.readLine(); //因为是多态 bufferedReader 可以接收 ZsA 的实例 ZsB的实例 ZsC的实例 所以就有了继续增强的效果，这里是拿着实现类的实现接着实现。
+		if(rt != null){
+			rt = 'A'+rt;
+		}
+		return rt;
+	}
+ *
  */
 package always_revision;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 
 /**
  * @author www.23.com
@@ -33,6 +75,58 @@ public class DesignModel {
 		// TODO Auto-generated method stub
 		/*tmp1 tp = new tmp1();
 		tp.getRunCodeTime();*/
+		
+		
+		//装饰着设计模式
+		//这样做的弊端就是每扩展一个功能就要去扩展一个类，造成类的泛滥。
+		/*Zs zs = null;
+		try {
+			zs = new Zs(new FileReader("d:/test2.txt"));
+			String tmp = "";
+			while((tmp = zs.readLine()) != null){
+				System.out.println(tmp);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			try {
+				zs.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}*/
+		
+		//完整的装饰着设计模式。
+		//思想就是利用多态，在每个子增强类上，维护者要增强类的引用。
+		//现在有三个类A B C 功能是在BufferedReader 的readLine上多打印A B C 如果维护着被增强类的引用就可以接着调用，三个类完成7个功能。
+		//A B C  AB AC BC ABC 
+		//这个例子就是完成ABC的
+		/*ZsA zsa = null;
+		try {
+			zsa = new ZsA(new ZsB(new ZsC(new BufferedReader(new FileReader("d:/test2.txt")))));
+			String tmp = "";
+			while((tmp = zsa.readLine()) != null){
+				System.out.println(tmp);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			try {
+				zsa.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}*/
 	}
 
 }
@@ -89,5 +183,71 @@ class tmp1 extends Template{
 	
 }
 
+
+//装饰着设计模式
+class Zs extends BufferedReader{
+	private int readCount;
+	public Zs(Reader in) {
+		super(in);
+		// TODO Auto-generated constructor stub	
+	}
+	
+	public String readLine() throws IOException{
+		String rt = super.readLine();
+		if(rt != null){
+			rt = ++readCount + rt;
+		}
+		return  rt;
+	}
+
+}
+//现在要扩展的是BufferedReader 的readLine方法。
+class ZsA extends BufferedReader{
+	private BufferedReader bufferedReader;
+	public ZsA(BufferedReader in) {//Reader是所有字符流的基类
+		super(in);
+		this.bufferedReader = in;
+		// TODO Auto-generated constructor stub
+	}
+	public String readLine() throws IOException{
+		String rt = bufferedReader.readLine();
+		if(rt != null){
+			rt = 'A'+rt;
+		}
+		return rt;
+	}
+}
+class ZsB extends BufferedReader{
+	private BufferedReader bufferedReader;
+	public ZsB(BufferedReader in) {
+		super(in);
+		this.bufferedReader = in;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String readLine() throws IOException{
+		String rt = bufferedReader.readLine();
+		if(rt != null){
+			rt = 'B'+rt;
+		}
+		return rt;
+	}
+}
+
+class ZsC extends BufferedReader{
+	private BufferedReader bufferedReader;
+	public ZsC(BufferedReader in) {
+		super(in);
+		this.bufferedReader = in;
+		// TODO Auto-generated constructor stub
+	}
+	public String readLine() throws IOException{
+		String rt = bufferedReader.readLine();
+		if(rt != null){
+			rt = 'C'+rt;
+		}
+		return rt;
+	}
+}
 
 
