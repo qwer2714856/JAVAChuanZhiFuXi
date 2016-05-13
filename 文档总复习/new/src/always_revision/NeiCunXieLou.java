44faab6cf17056355c8f1ae8b82e7eaa8e2eb4ab
luchao.ding <116710782@qq.com>
2016-05-13 15:26:55 +0800

    'neicun'

---
+++ 文档总复习/new/src/always_revision/NeiCunXieLou.java
@@ -0,0 +1,23 @@
+/**
+ * 内存泄露
+ * 1.对象用完了将一用这赋值null 让GC回收它
+ * Object c = new Object();
+ * c = null;
+ */
+package always_revision;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class NeiCunXieLou {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+
+	}
+
+}
