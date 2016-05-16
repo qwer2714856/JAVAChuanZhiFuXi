a6d5c12e2db0ec1caf81ac7f8fb52ea507553549
luchao.ding <116710782@qq.com>
2016-05-16 08:53:12 +0800

    'FanShe'

---
+++ 文档总复习/new/src/always_revision/FanShe.java
@@ -0,0 +1,20 @@
+/**
+ * ∑¥…‰
+ */
+package always_revision;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class FanShe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
