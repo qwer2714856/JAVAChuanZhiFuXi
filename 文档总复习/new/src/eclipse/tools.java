9f067f6f88ae74bf18cd6622da8bd273539b890f
luchao.ding <116710782@qq.com>
2016-05-18 16:54:50 +0800

    'eclipse_tools'

---
+++ 文档总复习/new/src/eclipse/tools.java
@@ -0,0 +1,32 @@
+/**
+ * IBM 开发的 
+ * 体系结构
+ * 
+ * 通过各种的插件可以写各种的语言，前提是安装了该该插件。（UML 都可以搞。。。）
+ * 问题 复杂度很高。
+ * 在安装开发中比较常用的。
+ * 
+ * 中作空间 --项目的结合
+ * 针对不同的项目类型，建立不同的工作空间。
+ * 切换工作空间 file->switch workspace other  切换就可以了如果切换过会有已经切换的目录。 注意这个目录可以是空的（新的工作空间）。
+ * 
+ * 
+ * 
+ */
+package eclipse;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class tools {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
