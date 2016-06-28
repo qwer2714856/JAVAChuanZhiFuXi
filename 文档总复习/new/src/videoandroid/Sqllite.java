4befe275bd035f3fb92a68ae0fd41c6513c566f1
luchao.ding <116710782@qq.com>
2016-06-28 08:46:32 +0800

    'sqlite'

---
+++ 文档总复习/new/src/videoandroid/Sqllite.java
@@ -0,0 +1,29 @@
+/**
+ * android 自带数据库sqllite
+ * 1.创建一个数据库类，这个类需要继承android.database.sqlite.SQLiteOpenHelper
+ * 创建的时候设置一下superclass 为android.database.sqlite.SQLiteOpenHelper
+ * 使用jutil 测试数据库
+ * 
+ * 项目看sqliteLx
+ * jutilTestSqlite
+ * MyDatabase
+ * 
+ * 
+ */
+package videoandroid;
+
+/**
+ * @author www.23.com
+ *
+ */
+public class Sqllite {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
